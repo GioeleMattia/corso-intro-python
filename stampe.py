5892aa0a9bc459cf59e50baced06c866c1491d7a
Ospite <gioele.scollo@gmail.com>
2017-02-23 16:00:26 +0100

    Un sacco di stampe

--- stampe.py
+++ stampe.py
@@ -14,4 +14,31 @@ print 3 > 4
 
 print 3 < 10
 
+name = "Gioele"
+age = 18
+delta = 5
+template = "Ciao, mi chiamo %s e %s anni fa avevo %s anni"
+
+sentence1 = "Ciao mamma guarda"
+sentence2 = "come mi diverto"
+
+
+print "Ciao, mi chiamo %s e ho %s anni" % (name, age)
+print "Ciao, sono %s" % name
+
+print "Ciao, mi chiamo %s e 10 anni fa avevo %s anni" % (name, age -10)
+
+print "Ciao, mi chiamo %s e %s anni fa avevo %s anni" % (name, delta, age - delta)
+
+print template % (name, delta, age - delta)
+
+print sentence1 + " " + sentence2
+
+print """
+Lorem ipsum
+dolor sit amet
+"""
+
+print "Ciao, \n Mi chiamo Gioele \n \n e ho 18 anni."
+
 
